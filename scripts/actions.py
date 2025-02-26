4213623bb0e13b3d41f50a3a52f1a852af0e3d8f
antalaveucm <162040505+antalaveucm@users.noreply.github.com>
2025-02-27 00:48:45 +0100

    0.1.12 - Added Equipment

--- scripts/actions.py
+++ scripts/actions.py
@@ -72,7 +72,8 @@ class ItemAction(Action):
 
     def perform(self) -> None:
         """Invoke the items ability, this action will be given to provide context."""
-        self.item.consumable.activate(self)
+        if self.item.consumable:
+            self.item.consumable.activate(self)
 
 class ActionWithDirection(Action):
     def __init__(self, entity: Actor, dx: int, dy: int):
@@ -151,7 +152,19 @@ class BumpAction(ActionWithDirection):
 
 class DropItem(ItemAction):
     def perform(self) -> None:
+        if self.entity.equipment.item_is_equipped(self.item):
+            self.entity.equipment.toggle_equip(self.item)
+
         self.entity.inventory.drop(self.item)
+
+class EquipAction(Action):
+    def __init__(self, entity: Actor, item: Item):
+        super().__init__(entity)
+
+        self.item = item
+
+    def perform(self) -> None:
+        self.entity.equipment.toggle_equip(self.item)
 
 class WaitAction(Action):
     def perform(self) -> None:
